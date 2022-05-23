70c700d18a33b321c605e717f6712bb6e215b832
martintoledoo <martinnfcn@gmail.com>
2022-05-23 17:34:34 -0300

    Etapa 3 y Parte de la 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    ArticulosFamiliasComponent
+    ArticulosFamiliasComponent,
+    MenuComponent,
+    ArticulosComponent
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+      { path: 'articulos', component: ArticulosComponent }
+      ])
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: "/" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
